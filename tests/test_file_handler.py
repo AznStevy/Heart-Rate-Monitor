2f162022ad92e0fa5c203a3e6016d500d7e437ba
Stephen <szx2@duke.com>
2018-10-20 02:33:21 -0400

    Uses pytest.raises to catch bad inputs in separate tests. Thanks Suyash! (closes #3)

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -6,19 +6,16 @@ from file_handler import FileHandler
 @pytest.mark.parametrize("filename, expected", [
     ("test1.json", ""),
     ("tests/test1.csv", "/tests"),
-    ("test_folder/test_sub_folder/test1.csv", "/test_folder/test_sub_folder")])
+    ("tests/test_data/test_data1.csv", "/tests/test_data")])
 def test_get_folder_path(filename, expected):
-    try:
-        ecg_file = FileHandler(filename, initialize=False)
-        assert ecg_file.get_folder_path() == expected
-    except FileNotFoundError:
-        assert True
+    ecg_file = FileHandler(filename, initialize=False)
+    assert ecg_file.get_folder_path() == expected
 
 
 @pytest.mark.parametrize("filename, expected", [
     ("test1.json", "test1"),
     ("tests/test1.json", "test1"),
-    ("tests/test_data/test1.csv", "test1")])
+    ("tests/test_data/test_data1.csv", "test_data1")])
 def test_get_basename(filename, expected):
     ecg_file = FileHandler("tests/test_data/test1.csv", initialize=False)
     assert ecg_file.get_basename(filename) == expected
@@ -27,38 +24,47 @@ def test_get_basename(filename, expected):
 @pytest.mark.parametrize("filename, expected", [
     ("test1.json", ".json"),
     ("tests/test1.json", ".json"),
-    ("test_folder/test_sub_folder/test1.csv", ".csv")])
+    ("tests/test_data/test_data1.csv", ".csv")])
 def test_get_ext(filename, expected):
     ecg_file = FileHandler(filename, initialize=False)
     assert ecg_file.get_ext() == expected
 
 
 @pytest.mark.parametrize("filename", [
-    ("tests/test1.csv"),
-    ("tests/thisdoesntexist.csv"),
-    ("tests/test1.dat")])
+    ("tests/test_data/test_data1.csv")])
 def test_read_data(filename):
     ecg_file = FileHandler(filename, initialize=False)
-    try:
+    time, signal = ecg_file.read_data()
+    assert len(time) == len(signal)
+
+
+@pytest.mark.parametrize("filename", [
+    ("tests/thisdoesntexist.csv"),
+    ("tests/test1.dat")])
+def test_read_data_no_file(filename):
+    ecg_file = FileHandler(filename, initialize=False)
+    with pytest.raises(FileNotFoundError):
         time, signal = ecg_file.read_data()
-        assert len(time) == len(signal)
-    except FileNotFoundError:
-        assert True
 
 
 @pytest.mark.parametrize("data", [
     ({}),
     ({"name": "Bob"}),
-    ({"_id": "42", "favorite_nums": [2, 3, 4]}),
-    (str("This is a string"))])
+    ({"_id": "42", "favorite_nums": [2, 3, 4]})])
 def test_write_data(data):
     test_file_path = "tests/test_data/test_json.json"
     ecg_file = FileHandler("/test_data/test1.csv", initialize=False)
-    try:
-        ecg_file.write_data(data, test_file_path)
-    except TypeError:
-        assert True
+    ecg_file.write_data(data, test_file_path)
 
     with open(test_file_path) as json_data:
         file_data = json.load(json_data)
         assert file_data == data
+
+
+@pytest.mark.parametrize("data", [
+    (str("This is a string"))])
+def test_write_bad_data(data):
+    test_file_path = "tests/test_data/test_json.json"
+    ecg_file = FileHandler("/test_data/test1.csv", initialize=False)
+    with pytest.raises(TypeError):
+        ecg_file.write_data(data, test_file_path)
